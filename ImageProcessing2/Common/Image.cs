2688c55994d2816f600fcc06fdd4a0c5b4e5738e
Artem Yur <yur.artem1995@gmail.com>
2017-04-30 18:26:34 +0300

    Revert "curse 2 - lab 2 - v. 1"
    This reverts commit 6fdc41150f7a33978756d23c4b03a7cea8998be7.

--- ImageProcessing2/Common/Image.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ImageProcessing2.Common
-{
-    public class Image
-    {
-        public byte[] ImageArray { get; set; }
-        public int Width { get; set; }
-        public int Height { get; set; } 
-    }
-}
--- ImageProcessing2/MainForm.HandlingGl.cs
+++ ImageProcessing2/MainForm.HandlingGl.cs
@@ -1,144 +1,100 @@
 ﻿using System;
 using System.Drawing;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using ImageProcessing2.Common;
 using OpenTK.Graphics.OpenGL;
-using Image = ImageProcessing2.Common.Image;
-using KeyPressEventArgs = System.Windows.Forms.KeyPressEventArgs;
 
 namespace ImageProcessing2
 {
     public partial class MainForm
     {
-        private Image _image;
         private static int _texId;
         private static bool _textureRendered;
-        private TransformationObject _transformationObject;
 
-        private static readonly DicomImageProvider DicomImageProvider = new DicomImageProvider();
+        private static RgbImageProvider _rgbImageProvider;
 
         private async void glControl_Load(object sender, EventArgs e)
         {
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
             GL.Enable(EnableCap.Texture2D);
 
-            _image = await DicomImageProvider.LoadDcmImage();
+            _rgbImageProvider = new RgbImageProvider();
 
-            ResizeWindow(_image.Width * 2, _image.Height * 2);
+            var image = await _rgbImageProvider.GetOriginalImage();
+            
+            ResizeWindow(image.Width, image.Height);
 
             this.Invoke(new MethodInvoker(glControl.Refresh));
 
             glControl.SwapBuffers();
         }
 
-        private void glControl_Paint(object sender, PaintEventArgs e)
+        private async void glControl_Paint(object sender, PaintEventArgs e)
         {
-            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-
-            if (!_textureRendered && _image != null)
+            if (_rgbImageProvider != null)
             {
-                GL.Ortho(-glControl.Width / 2, glControl.Width / 2, -glControl.Height / 2, glControl.Height / 2, -1.0, 1.0);
-                GenerateAndBindTexture();
-                _textureRendered = true; GL.LoadIdentity();
-            }
+                GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
-            OnRenderFrame(e);
-            glControl.SwapBuffers();
-        }
-
-        private void glControl_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            if (e.KeyChar == Convert.ToChar(Keys.Q))
-            {
-                var transformationObj = new TransformationObject();
-
-                var answer = Prompt.ShowDialog("Enter shear angle", "Shear");
-                transformationObj.ShearAngle = Convert.ToDouble(answer);
-
-                answer = Prompt.ShowDialog("Enter value for x reference line", "Shear reference line");
-                transformationObj.ShearXReferenceLine = Convert.ToDouble(answer);
-
-                var shearMatrix =
-                    MatrixHelper.GetShearMatrix(transformationObj.ShearAngle, transformationObj.ShearXReferenceLine);
-
-                answer = Prompt.ShowDialog("Enter rotation angle", "Rotation");
-                transformationObj.RotationAngle = Convert.ToDouble(answer);
-
-                answer = Prompt.ShowDialog("Enter x coordinate", "Rotation");
-                transformationObj.RotationXPoint = Convert.ToDouble(answer);
-
-                answer = Prompt.ShowDialog("Enter y coordinate", "Rotation");
-                transformationObj.RotationYPoint = Convert.ToDouble(answer);
-
-                var rotationMatrix =
-                    MatrixHelper.GetRotationMatrix(
-                        transformationObj.RotationAngle,
-                        transformationObj.RotationXPoint,
-                        transformationObj.RotationYPoint);
-
-                transformationObj.TransformationMatrix =
-                    MatrixHelper.MatrixMultiplication(shearMatrix, rotationMatrix);
-
-                _transformationObject = transformationObj;
-            }
-
-            if (e.KeyChar == Convert.ToChar(Keys.W))
-            {
-                if (_transformationObject != null)
+                if (!_textureRendered)
                 {
-                    var invShearMatrix = MatrixHelper.GetInverseShearMatrix(
-                                            _transformationObject.ShearAngle,
-                                            _transformationObject.ShearXReferenceLine);
-
-                    var invRotationMatrix = MatrixHelper.GetInverseRotationMatrix(
-                                                _transformationObject.RotationAngle,
-                                                _transformationObject.RotationXPoint,
-                                                _transformationObject.RotationYPoint);
-
-                    var invTransformationMatrix = MatrixHelper.MatrixMultiplication(invRotationMatrix, invShearMatrix);
-
-                    _transformationObject.TransformationMatrix = invTransformationMatrix;
+                    await GenerateAndBindTexture();
+                    _textureRendered = true;
                 }
-            }
 
-            this.Invoke(new MethodInvoker(glControl.Refresh));
+                OnRenderFrame(e);
+                glControl.SwapBuffers();
+            }
         }
 
-        private void GenerateAndBindTexture()
+        private async void glControl_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (_rgbImageProvider != null)
+            {
+                if (e.KeyChar == Convert.ToChar(Keys.Q))
+                    BindTexture(await _rgbImageProvider.GetOriginalImage());
+
+                if (e.KeyChar == Convert.ToChar(Keys.W))
+                    BindTexture(await _rgbImageProvider.GetDisjunctionOfOriginalImageWithBackgroundBuffer());
+
+                if (e.KeyChar == Convert.ToChar(Keys.E))
+                    BindTexture(await _rgbImageProvider.GetGradientedOriginalImage());
+
+                this.Invoke(new MethodInvoker(glControl.Refresh));
+            }
+        }
+
+        private async Task GenerateAndBindTexture()
         {
             GL.GenTextures(1, out _texId);
-            BindTexture(_image);
+            BindTexture(await _rgbImageProvider.GetOriginalImage());
         }
-
-        private void BindTexture(Image image, PixelInternalFormat pixelInternalFormat = PixelInternalFormat.Luminance,
-            PixelFormat pixelFormat = PixelFormat.Luminance)
+        
+        private void BindTexture(RgbImage<byte> rgbImage)
         {
             GL.BindTexture(TextureTarget.Texture2D, _texId);
             GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
-            GL.TexImage2D(TextureTarget.Texture2D, 0, pixelInternalFormat, image.Width, image.Height, 0,
-                pixelFormat, PixelType.UnsignedByte, image.ImageArray);
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgb, rgbImage.Width, rgbImage.Height, 0,
+                PixelFormat.Rgb, PixelType.UnsignedByte, rgbImage.ImmageArray);
         }
-
+        
         private void OnRenderFrame(PaintEventArgs e)
         {
-            GL.MatrixMode(MatrixMode.Projection);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
 
-            if (_transformationObject != null)
-            {
-                GL.MultMatrix(_transformationObject.TransformationMatrix);
-            }
+            GL.BindTexture(TextureTarget.Texture2D, _texId);
 
             GL.Begin(BeginMode.Quads);
 
-            GL.TexCoord2(1.0f, 0.0f); GL.Vertex2(0.5f, -0.5f);
-            GL.TexCoord2(1.0f, 1.0f); GL.Vertex2(-0.5f, -0.5f);
-            GL.TexCoord2(0.0f, 1.0f); GL.Vertex2(-0.5f, 0.5f);
-            GL.TexCoord2(0.0f, 0.0f); GL.Vertex2(0.5f, 0.5f);
+            GL.TexCoord2(1.0f, 0.0f); GL.Vertex2(1f, -1f);
+            GL.TexCoord2(1.0f, 1.0f); GL.Vertex2(1f, 1f);
+            GL.TexCoord2(0.0f, 1.0f); GL.Vertex2(-1f, 1f);
+            GL.TexCoord2(0.0f, 0.0f); GL.Vertex2(-1f, -1f);
 
             GL.End();
-            GL.Flush();
         }
-
+        
         private void ResizeWindow(int width, int height)
         {
             if (width >= _maxWidth || height >= _maxHeight)
@@ -154,16 +110,6 @@ namespace ImageProcessing2
                     this.ClientSize = new Size(width, height);
                 }));
             }
-        }
-
-        class TransformationObject
-        {
-            public double[] TransformationMatrix;
-            public double ShearAngle;
-            public double ShearXReferenceLine;
-            public double RotationAngle;
-            public double RotationXPoint;
-            public double RotationYPoint;
         }
     }
 }
